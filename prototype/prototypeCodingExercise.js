f4de421b0c84aa8c24ddda121957e11fbbb28315
Christopher <christiller2015@gmail.com>
2020-12-10 22:57:55 -0500

    Complete prototype section

---
+++ prototype/prototypeCodingExercise.js
@@ -0,0 +1,29 @@
+// No, I'm not implementing the entire deep copy method for this problem. Tyvm.
+
+class Point
+{
+  constructor(x, y)
+  {
+    this.x = x;
+    this.y = y;
+  }
+  
+  deepCopy()
+  {
+      return new Point(this.x, this.y);
+  }
+}
+
+class Line
+{
+	constructor(start, end)
+  {
+    this.start = start;
+    this.end = end;
+  }
+
+  deepCopy()
+  {
+      return new Line(this.start.deepCopy(), this.end.deepCopy());
+  }
+}
